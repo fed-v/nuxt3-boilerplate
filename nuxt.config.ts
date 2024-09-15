ca5541623ef17afc69749b6c8ea98fba3806d0e2
Federico <fed_v@hotmail.com>
2024-09-15 10:53:24 -0400

    Added new eslint Nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,125 +1,113 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { 
-    enabled: true 
+  devtools: {
+    enabled: true,
   },
 
-  modules: [
-    '@pinia/nuxt', 
-    "@nuxtjs/i18n"
-  ],
+  modules: ['@pinia/nuxt', '@nuxtjs/i18n', '@nuxt/eslint'],
 
   imports: {
     // Auto-import pinia stores defined in `~/stores`
-    dirs: ['stores']
+    dirs: ['stores'],
   },
 
   pinia: {
-    autoImports: [
-      'defineStore',
-      'storeToRefs',
-    ],
+    autoImports: ['defineStore', 'storeToRefs'],
   },
 
   i18n: {
     /* module options */
     lazy: true,
-    langDir: "locales",
-    strategy: "prefix_except_default",
+    langDir: 'locales',
+    strategy: 'prefix_except_default',
     locales: [
       {
-        code: "en-US",
-        iso: "en-US",
-        name: "English(US)",
-        file: "en-US.json",
+        code: 'en-US',
+        iso: 'en-US',
+        name: 'English(US)',
+        file: 'en-US.json',
       },
       {
-        code: "fr-FR",
-        iso: "fr-FR",
-        name: "French(FR)",
-        file: "fr-FR.json",
-      }
+        code: 'fr-FR',
+        iso: 'fr-FR',
+        name: 'French(FR)',
+        file: 'fr-FR.json',
+      },
     ],
-    defaultLocale: "en-US",
+    defaultLocale: 'en-US',
   },
 
   css: [
     '@/assets/styles/reset.css',
     '@/assets/styles/variables.css',
-    '@/assets/styles/global.css'
+    '@/assets/styles/global.css',
   ],
 
   app: {
-
     // Define the global head meta tags
     head: {
-
       // Set global metadata for SEO
       title: 'Site Title',
 
       meta: [
-        { 
-            name: 'description', content: 'Site Description'
-        }
+        {
+          name: 'description',
+          content: 'Site Description',
+        },
       ],
-      
+
       // Load external Javascript file
-      script: [ 
-        { 
-          src: 'https://unpkg.com/ionicons@latest/dist/ionicons/ionicons.js'
-        } 
+      script: [
+        {
+          src: 'https://unpkg.com/ionicons@latest/dist/ionicons/ionicons.js',
+        },
       ],
 
       // Load external stylesheets
-      link: [ 
+      link: [
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
-        } 
-      ]
-    }
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap',
+        },
+      ],
+    },
   },
 
   // Any configuration settings that only run in Dev mode go here:
   $development: {
     app: {
-        head: {    
-            title: 'Dev | Title'
-        }
-    }
+      head: {
+        title: 'Dev | Title',
+      },
+    },
   },
 
   // Any configuration settings that only run in Production go here:
   $production: {
-      app: {
-          head: {    
-              title: 'Prod | Title'
-          }
-      }
+    app: {
+      head: {
+        title: 'Prod | Title',
+      },
+    },
   },
 
   // Any configuration settings that only run when testing go here:
   $test: {
-      app: {
-          head: {    
-              title: 'Test | Title'
-          }
-      }
+    app: {
+      head: {
+        title: 'Test | Title',
+      },
+    },
   },
 
   // Runtime variables
-  runtimeConfig : {
-
+  runtimeConfig: {
     // Private variables
-    
 
     public: {
-
       // Public variables
-
-    }
-    
+    },
   },
 
   compatibilityDate: '2024-08-28',
-})
+});
